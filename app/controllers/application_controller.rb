9e7b14597be93cbd2921d751eeed2ee7d2562c21
github username <github email address>
2020-03-16 22:36:08 -0400

    Added untracked files

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :view, Proc.new { File.join(root, "../views/") }
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get '/registrations/signup' do
+    erb :'/registrations/signup'
+  end
+
+  # post '/registrations' do
+  #   @user = User.new(name: params["name", email: params["email"], password: params["password"])
+  # end
+
+end
