a2b7aa9ce2af99a8421514b008b020ed393c3fb2
github username <github email address>
2020-03-19 10:40:07 -0400

    Change users_controller name to singular

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+require './config/environment'
+
+class UserController < ApplicationController
+
+
+    
+end
--- app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-require './config/environment'
-
-class UsersController < Sinatra::Base
-
-
-    
-end
