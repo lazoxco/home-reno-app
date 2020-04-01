cb220db9b29fd06d8121d3999e292eea8cef5c62
github username <github email address>
2020-03-31 22:52:47 -0400

    Complete.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,23 @@ class SessionsController < ApplicationController
         erb :'/users/login'
       end
     
-      post '/login' do
-        # want to find the user if it exists
-        @user = User.find_by(email: params[:email])
-    
-        if @user && @user.authenticate(params[:password])
-          # authenticate password
-          session[:user_id] = @user.id
-          redirect "/users/#{@user.id}"
-        else
-          redirect "/login"
-        end
-    
+    post '/login' do
+      # want to find the user if it exists
+      @user = User.find_by(email: params[:email])
+  
+      if @user && @user.authenticate(params[:password])
+        # authenticate password
+        session[:user_id] = @user.id
+        # redirect "/users/#{@user.id}"
+        redirect "/tasks"
+      else
+        redirect '/login'
       end
+    end
 
-      get '/logout' do
-        session.clear
-        redirect '/'
-      end
+    get '/logout' do
+      session.clear
+      redirect '/'
+    end
 
 end
